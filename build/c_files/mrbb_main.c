60c647b2f495806b0f8d401c2d7f9102a4a09ce2
Jan Berdajs <mrbrdo@gmail.com>
2012-12-11 22:45:39 +0100

    cleanup runner func

--- build/c_files/mrbb_main.c
+++ build/c_files/mrbb_main.c
@@ -1,11 +1,9 @@
-
-void mrbb_main(mrb_state *mrb) {
-  int a = 0;
+mrb_value mrbb_main(mrb_state *mrb) {
   mrb_callinfo *ci;
   mrb_value recv = mrb_top_self(mrb);
   struct RProc *p;
   int ai = mrb->arena_idx;
-  jmp_buf *prev_jmp = (jmp_buf *)mrb->jmp;
+  jmp_buf *prev_jmp = mrb->jmp;
   jmp_buf c_jmp;
   mrb_value result;
 
@@ -13,28 +11,25 @@ void mrbb_main(mrb_state *mrb) {
     mrb->jmp = &c_jmp;
   }
   else {
-    printf("unexpected exception ");
+    mrb->jmp = prev_jmp;
+    printf("Uncaught exception:\n");
     mrb_p(mrb, mrb_obj_value(mrb->exc));
-    exit(0);
-    mrbb_raise(mrb, prev_jmp); // TODO ?
+    return;
   }
 
   if (!mrb->stack) {
     stack_init(mrb);
   }
 
-  // TODO: patch Proc
+  // Path Proc
   {
     struct RProc *m = mrb_proc_new_cfunc(mrb, mrbb_proc_call);
     mrb_define_method_raw(mrb, mrb->proc_class, mrb_intern(mrb, "call"), m);
     mrb_define_method_raw(mrb, mrb->proc_class, mrb_intern(mrb, "[]"), m);
   }
-  // END TODO
 
-  mrb->stack[0] = recv;
   /* prepare stack */
   ci = cipush(mrb);
-  //ci->pc = pc + 1;
   ci->acc = -1;
   ci->mid = 0;
   ci->stackidx = mrb->stack - mrb->stbase;
@@ -42,26 +37,18 @@ void mrbb_main(mrb_state *mrb) {
   ci->target_class = mrb_class(mrb, recv);
 
   /* prepare stack */
-  mrb->stack += a;
+  mrb->stack[0] = recv;
 
-  p = mrb_proc_new_cfunc(mrb, rb_main);
-  // p = mrb_proc_new(mrb, mrb->irep[irep->idx+GETARG_Bx(i)]);
+  p = mrbb_proc_new(mrb, rb_main);
   p->target_class = ci->target_class;
   ci->proc = p;
 
-  // if (MRB_PROC_CFUNC_P(p)) {
-  // else part removed since it is always CFUNC
-
-  //mrb_gc_protect(mrb, mrb_obj_value(ci->proc)); // TODO just testing...
-  //mrb->stack[0] =
   result = p->body.func(mrb, recv);
-  mrb_gc_protect(mrb, result);
   mrb->arena_idx = ai;
+  mrb_gc_protect(mrb, result);
+
   mrb->stack = mrb->stbase + mrb->ci->stackidx;
   cipop(mrb);
-  mrb->stack[0] = result;
-  //if (mrb->exc) mrbb_raise(mrb, prev_jmp); // TODO
-  /* pop stackpos */
-  //mrb->stack = mrb->stbase + mrb->ci->stackidx;
-  //cipop(mrb);
+
+  return result;
 }
---
+++ testsuite.rb
@@ -0,0 +1,19 @@
+# run mruby test suite
+
+#include 'mruby/mrblib/array.rb'
+#include 'mruby/mrblib/enum.rb'
+#include 'mruby/mrblib/class.rb'
+#include 'mruby/mrblib/compar.rb'
+#include 'mruby/mrblib/error.rb'
+#include 'mruby/mrblib/hash.rb'
+#include 'mruby/mrblib/kernel.rb'
+#include 'mruby/mrblib/numeric.rb'
+#include 'mruby/mrblib/print.rb'
+#include 'mruby/mrblib/range.rb'
+#include 'mruby/mrblib/string.rb'
+#include 'mruby/mrblib/struct.rb'
+
+#include 'mruby/test/assert.rb'
+#include 'mruby/test/t/*.rb'
+
+report
