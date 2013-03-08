ac0a293e517744d9013c8c884c700a148ef09e19
MrBrdo <mrbrdo@gmail.com>
2013-03-08 17:02:23 +0100

    some performance testing and comparison

---
+++ run_performance_test.rb
@@ -0,0 +1,43 @@
+require 'stringio'
+require_relative './mrbcc/preparser'
+# preparse
+rb_filename = File.expand_path('../test/performance_test.rb', __FILE__)
+rb_filename_noext = File.basename(rb_filename, ".rb")
+Preparser.preparse(rb_filename, "tmp/tmp_out.rb")
+
+# run
+durations_separator = 'Durations:'
+
+output = %x[mruby/bin/mruby tmp/tmp_out.rb]
+idx = output.index(durations_separator)
+perf_mruby = if idx
+  eval(output[idx + durations_separator.length, output.length].strip)
+end
+
+%x[./mrbcc_compile test/performance_test.rb]
+output = %x[./runner test/performance_test.so]
+idx = output.index(durations_separator)
+perf_mruby_cc = if idx
+  eval(output[idx + durations_separator.length, output.length].strip)
+end
+
+diff = perf_mruby.keys.map do |key|
+  perf_data = {
+    mruby: perf_mruby[key],
+    mruby_cc: perf_mruby_cc[key]
+  }
+  [key, perf_data]
+end
+diff = Hash[diff]
+
+diff_percent = diff.each_pair.map do |key, data|
+  [key, data[:mruby_cc] / data[:mruby].to_f]
+end
+
+diff_percent.sort! { |a,b| b[1] <=> a[1] }
+
+diff_percent.each do |data|
+  key = data[0]
+  puts key
+  puts (data[1] * 100).ceil.to_s + "% mruby: #{perf_mruby[key]}, mruby_cc: #{perf_mruby_cc[key]}"
+end
---
+++ test/performance_test.rb
@@ -0,0 +1,26 @@
+N_EXAMPLE_ITERATIONS = 10000
+N_SUITE_ITERATIONS = 1
+
+$example_durations = Hash.new
+
+#include '../mruby/test/assert.rb'
+self.class.alias_method :assert_orig, :assert
+def assert(str = 'Assertion failed', iso = '', &block)
+  i = 0
+  start_at = Time.now
+  assert_orig(str, iso, &block) while (i += 1) <= N_EXAMPLE_ITERATIONS
+  duration = Time.now - start_at
+
+  str += ", #{iso}" unless iso.empty?
+  $example_durations[str] = duration
+end
+
+_suite_iterations = 0
+_suite_start_at = Time.now
+while (_suite_iterations += 1) <= N_SUITE_ITERATIONS
+#include '../mruby/test/t/*.rb'
+end
+$example_durations['ALL'] = Time.now - _suite_start_at
+
+puts "Durations:"
+p $example_durations
