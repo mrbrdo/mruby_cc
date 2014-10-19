9805d9a69c298f4fae5d3e5fc0bb4036750e85d2
Jan Berdajs <mrbrdo@gmail.com>
2014-10-19 18:49:01 +0200

    automatically include enabled mrbgems ruby code in compilation

--- mrbcc_mrblib/compile_mrblib.rb
+++ mrbcc_mrblib/compile_mrblib.rb
@@ -1,9 +1,26 @@
 require 'fileutils'
 
+def write_gem_rb_code(outf, name)
+  Dir[File.expand_path("../../mruby/mrbgems/mruby-#{name}/mrblib/*.rb", __FILE__)].each do |fn|
+    outf.write(File.read(fn))
+  end
+end
+
 tmp_fn = File.expand_path("../../tmp_mrblib.rb", __FILE__)
 File.open(tmp_fn, "w") do |outf|
   Dir[File.expand_path("../../mruby/mrblib/*.rb", __FILE__)].each do |fn|
     outf.write(File.read(fn))
+  end
+
+  # mrbgems
+  ordered_mrbgems = %w{sprintf print}
+  ordered_mrbgems.each { |name| write_gem_rb_code(outf, name) }
+  Dir[File.expand_path("../../mruby/build/host/mrbgems/mruby-*", __FILE__)].each do |fn|
+    if name = fn[/mruby-([^\/\\\.]+)\z/, 1]
+      next if ordered_mrbgems.include?(name)
+      puts name
+      write_gem_rb_code(outf, name)
+    end
   end
 end
 
